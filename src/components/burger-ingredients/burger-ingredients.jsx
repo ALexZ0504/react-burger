ca019f6984d830fc43d4642e56fb4052dbe6e3fb
AlexZ0504 <sasha.zakutajlo@mail.ru>
2024-11-20 12:48:18 +0300

    Черновой BurgIng готов, проблема с gap

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -4,14 +4,18 @@ import TabList from "./tab-list/tab-list";
 import { CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 import n200i from "../../images/N-200i.png";
 import r2d3 from "../../images/R2-D3.png";
+import spicyX from "../../images/Spicy-X.png";
+import space from "../../images/Space_Sause.png";
+import traditional from "../../images/Traditional_Sause.png";
+import antarian from "../../images/Antarian_Sause.png";
 
 function BurgerIngredients() {
   return (
     <section className={styles.container}>
       <h2 className={styles.title}>Соберите бургер</h2>
       <TabList />
-      <h3 className={styles.tabName}>Булки</h3>
       <div className={styles.items}>
+        <h3 className={styles.tabName}>Булки</h3>
         <div className={styles.item}>
           <img className={styles.img} src={n200i}></img>
           <div className={styles.priceBox}>
@@ -27,6 +31,41 @@ function BurgerIngredients() {
             <CurrencyIcon />
           </div>
           <p className={styles.description}>Флюоресцентная булка R2-D3</p>
+        </div>
+        <h3 className={styles.tabName}>Соусы</h3>
+        <div className={styles.item}>
+          <img className={styles.img} src={spicyX}></img>
+          <div className={styles.priceBox}>
+            <h3 className={styles.price}>30</h3>
+            <CurrencyIcon />
+          </div>
+          <p className={styles.description}>Соус Spicy-X</p>
+        </div>
+        <div className={styles.item}>
+          <img className={styles.img} src={space}></img>
+          <div className={styles.priceBox}>
+            <h3 className={styles.price}>30</h3>
+            <CurrencyIcon />
+          </div>
+          <p className={styles.description}>Соус фирменный Space Sauce</p>
+        </div>
+        <div className={styles.item}>
+          <img className={styles.img} src={traditional}></img>
+          <div className={styles.priceBox}>
+            <h3 className={styles.price}>30</h3>
+            <CurrencyIcon />
+          </div>
+          <p className={styles.description}>Соус традиционный галактический</p>
+        </div>
+        <div className={styles.item}>
+          <img className={styles.img} src={antarian}></img>
+          <div className={styles.priceBox}>
+            <h3 className={styles.price}>30</h3>
+            <CurrencyIcon />
+          </div>
+          <p className={styles.description}>
+            Соус с шипами Антарианского плоскоходца
+          </p>
         </div>
       </div>
     </section>
